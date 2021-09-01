bb1d5dbebb25c2e6d4a525611a6d9f3af9dc425a
Robert <robert.silver@outlook.com>
2021-09-01 11:19:44 +0100

    Removed the console log line and added & removed the selected class depending on which tab was chosen.

--- JS/maps-tabs.js
+++ JS/maps-tabs.js
@@ -7,17 +7,21 @@ function handleTabClick(event) {
   tabPanels.forEach(panel => {
     panel.hidden = true;
   });
+
   // mark all tabs as unselected
   tabButtons.forEach(tab => {
     // tab.ariaSelected = false;
     tab.setAttribute('aria-selected', false);
+    tab.classList.remove('selected');
   });
+
   // mark the clicked tab as selected
   event.currentTarget.setAttribute('aria-selected', true);
+  event.currentTarget.classList.add('selected');
+
   // find the associated tabPanel and show it!
   const { id } = event.currentTarget;
 
-  console.log(tabPanels);
   const tabPanel = tabPanels.find(
     panel => panel.getAttribute('aria-labelledby') === id
   );
